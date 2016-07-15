793313a26f1ff2ab578dabade34b24fffda9566c
Kai Yan <kaiyan.yeo@gmail.com>
2016-07-15 13:10:41 +0800

    Minor edits to pages

--- groupmatefinder.py
+++ groupmatefinder.py
@@ -19,15 +19,18 @@ from google.appengine.api import mail
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
 
-# Datastore definitions
-class User(ndb.Model):
-    email = ndb.StringProperty() # user email
-    id = ndb.StringProperty() # user's nickname
 
-"""class Profile(ndb.Model):
-    # Stores data about the user
-    gender = ndb.StringProperty()
-""" 
+# Datastore definitions
+class Student(ndb.Model):
+    # Sub model for representing a user
+    email = ndb.StringProperty() # user email
+    nickname = ndb.StringProperty() # user's nickname
+
+class Profile(ndb.Model):
+    # Model for representing a user's profile
+    student = ndb.StructuredProperty(Student)
+    work_time = ndb.StringProperty()
+
 
 # This is for the front page
 class HomePage(webapp2.RequestHandler):
@@ -123,7 +126,29 @@ class Profiling_Questions(webapp2.RequestHandler):
         else:
             template = jinja_environment.get_template('profiling_questions.html')
             self.response.out.write(template.render())
-            
+
+    def post(self):
+        user = users.get_current_user()
+
+        curr = ndb.Key('Profile', users.get_current_user().nickname())
+        profile = curr.get()
+#        if user:
+        profile.student = Student(
+            email = users.get_current_user(),
+            nickname = users.get_current_user().nickname())
+        # .get("Submit")?
+        work_time_ans = self.request.get('work_time')
+        profile.work_time = work_time_ans
+        profile.put()
+
+        template_values = {
+            'user_nickname': users.get_current_user().nickname(),
+            'logout': users.create_logout_url(self.request.host_url),
+            'work_time': profile.work_time,
+            }
+        template = jinja_environment.get_template('profiling_questions.html')
+        self.response.out.write(template.render(template_values))
+
 class Groups(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
